9c863de6bb9d4f5dab3dcc639a0efd12d6c7e837
asmckeithan <annesha.mckeithan@gmail.com>
2024-05-08 19:30:49 -0400

    commented and revie wed Part1

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ const adventurer = {
     },
   };
   adventurer.inventory.forEach((item) => {
-    console.log(item);
+    console.log(item);// this loop logs each item in Roins Invemtory
   });
   adventurer.companion.companion2 = {
     name: "Frank",
     type: "Flea",
     belongings: ["hat", "sunglasses"],
-  };
+  };//above we added a companion2 
   console.log(adventurer);
   adventurer.roll();
+
+  //we could find a sword at adventurer.inventory[0].
