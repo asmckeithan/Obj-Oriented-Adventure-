5a0e01cd83b1962ada02f64656d8c71ee909c512
asmckeithan <annesha.mckeithan@gmail.com>
2024-05-08 18:50:18 -0400

    new file

---
+++ index.js
@@ -0,0 +1,23 @@
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+      name: "Leo",
+      type: "Cat",
+    },
+    roll(mod = 0) {
+      const result = Math.floor(Math.random() * 20) + 1 + mod;
+      console.log(`${this.name} rolled a ${result}.`);
+    },
+  };
+  adventurer.inventory.forEach((item) => {
+    console.log(item);
+  });
+  adventurer.companion.companion2 = {
+    name: "Frank",
+    type: "Flea",
+    belongings: ["hat", "sunglasses"],
+  };
+  console.log(adventurer);
+  adventurer.roll();
